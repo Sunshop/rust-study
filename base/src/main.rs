ad694c8ff12fe34dd86e7bb322a1cbc1982b4448
李昊谦 <lihaoqian@znlh.com>
2023-05-19 14:15:44 +0800

    新增：结构体学习

--- base/src/main.rs
+++ base/src/main.rs
@@ -6,7 +6,6 @@ fn five() -> i32 {
 
 fn plus_one(x: i32) -> i32 {
     return x + 2;
-    x + 1
 }
 
 fn main() {
@@ -18,12 +17,22 @@ fn main() {
 
     let x = five();
 
+    let y = plus_one(x);
+
     println!("{x}");
-
-
-    let y = plus_one(9);
-
     println!("{y}");
+
+    // 可变引用
+    let mut a = String::from("hello");
+    change(&mut a);
+
+
+}
+
+// fn first_word() ->
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
 
 fn another_function(x: i32) {
