2547c9bedf7d2079ea9df619be35b2b6fec183d1
李昊谦 <lihaoqian@znlh.com>
2023-05-25 16:12:43 +0800

    修改：语法错误

--- enum_study/src/main.rs
+++ enum_study/src/main.rs
@@ -12,31 +12,30 @@ fn main() {
     
     let home = IpAddr {
         kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1");
-    }
+        address: String::from("127.0.0.1"),
+    };
     
     let loopback = IpAddr {
         kind: IpAddrKind::V6,
-        address: String.from("::1");
-    }
-};
-
+        address: String::from("::1"),
+    };
+}
 
 // 枚举与结构体结合
-fn main() {
-    enum Message {
-        Quit,
-        Move { x: i32, y: i32 },
-        Write(String),
-        ChangeColor(i32, i32, i32),
-    }
+// fn main() {
+//     enum Message {
+//         Quit,
+//         Move { x: i32, y: i32 },
+//         Write(String),
+//         ChangeColor(i32, i32, i32),
+//     }
 
-    impl Message {
-        fn call(&self) {
-            // 在这里定义方法体
-        }
-    }
+//     impl Message {
+//         fn call(&self) {
+//             // 在这里定义方法体
+//         }
+//     }
 
-    let m = Message::Write(String::from("hello"));
-    m.call();
-}
+//     let m = Message::Write(String::from("hello"));
+//     m.call();
+// }
