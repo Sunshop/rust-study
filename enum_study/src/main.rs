9844ec3ee10a1e32af049994ce01ef296ff61e7d
李昊谦 <lihaoqian@znlh.com>
2023-06-07 15:01:36 +0800

    新增：枚举学习

--- enum_study/src/main.rs
+++ enum_study/src/main.rs
@@ -5,20 +5,57 @@ fn main() {
         V6,
     }
     
-    struct IpAddr {
-        kind: IpAddrKind,
-        address: String,
+    // struct IpAddr {
+    //     kind: IpAddrKind,
+    //     address: String,
+    // }
+    
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
+    
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+
+    // fn route(ip_kind: IpAddrKind) {
+
+    // }
+
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let home2 = IpAddr::V4(192, 168, 0, 1);
+
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    fn route(ip_kind: IpAddr) {
+
     }
     
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    route(home2);
+
+    // enum Option<T> {
+    //     Some(T),
+    //     None,
+    // }
     
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    // let some_number = Option::Some(5);
+
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    if !y.is_none() {
+        println!("y is some");
+    }
+    let sum = x + y.unwrap(); // 取值
+    println!("{sum}");
 }
 
 // 枚举与结构体结合
